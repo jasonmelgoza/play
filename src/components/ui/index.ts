f722e4c6f8ea51ffff7b9f54461c273acec252e9
Jason Melgoza <jasonmelgoza@gmail.com>
2024-08-17 13:29:39 -0700

    feat: Add @radix-ui/react-aspect-ratio and @radix-ui/react-avatar components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,5 +3,6 @@ export { Alert, AlertTitle, AlertDescription } from './alert';
 export { AlertDialog, AlertDialogPortal, AlertDialogOverlay, AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogFooter, AlertDialogTitle, AlertDialogDescription, AlertDialogAction, AlertDialogCancel } from './alert-dialog';
 export { AspectRatio } from './aspect-ratio';
 export { Avatar, AvatarImage, AvatarFallback } from './avatar';
+export { Badge, badgeVariants } from './badge';
 export { Button, buttonVariants } from './button';
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card';
