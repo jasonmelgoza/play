8c4dda5989bcc7502e11562493c40408627e3471
Jason Melgoza <jasonmelgoza@gmail.com>
2024-09-15 20:03:13 -0700

    Refactor package.json to add styled-components dependency

--- playroom/FrameComponent.tsx
+++ playroom/FrameComponent.tsx
@@ -1,12 +1,21 @@
 import React, { useEffect } from 'react';
 import { ThemeProvider } from '../src/ThemeContext';
 import { lightTheme, darkTheme } from '../src/themes';
+import { createGlobalStyle } from 'styled-components';
 import '../src/styles/global.css';
 
 interface FrameComponentProps {
   children: React.ReactNode;
   theme: typeof lightTheme | typeof darkTheme;
 }
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700&display=swap');
+
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+`;
 
 const useBodyClass = (className: string) => {
   useEffect(() => {
@@ -19,7 +28,12 @@ const useBodyClass = (className: string) => {
 
 const FrameComponent: React.FC<FrameComponentProps> = ({ children, theme }) => {
   useBodyClass('antialiased');
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default FrameComponent;
