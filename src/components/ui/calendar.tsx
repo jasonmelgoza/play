7013c5827597fe2c0e06c6d33dd2ceddd7985745
Jason Melgoza <jasonmelgoza@gmail.com>
2024-09-23 23:59:25 -0700

    Fixed button path issue

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,7 +3,7 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@lib/utils"
-import { buttonVariants } from "src/components/ui/button"
+import { buttonVariants } from './button';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
